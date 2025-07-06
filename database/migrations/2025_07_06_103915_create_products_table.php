3c37f5516e60d8b15d3983d70815e872b7d7badb
Keith Jordan <shadow902@gmail.com>
2025-07-06 07:31:19 -0400

    update table migrations

--- database/migrations/2025_07_06_103915_create_products_table.php
+++ database/migrations/2025_07_06_103915_create_products_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string(name);
+            $table->string('slug');
+            $table->integer('qty');
+            $table->integer('price');
+            $table->integer('old_price')->nullable();
+            $table->integer('discount')->nullable();
+            $table->longText('description');
+            $table->string('thumbnail');
+            $table->string('first_image')->nullable();
+            $table->string('second_image')->nullable();
+            $table->string('third_image')->nullable();
+            $table->boolean('status')->default(1); 
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('subcategory_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('childcategory_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('brand_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
--- database/migrations/2025_07_06_111831_create_coupons_table.php
+++ database/migrations/2025_07_06_111831_create_coupons_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('subcategories', function (Blueprint $table) {
+        Schema::create('coupons', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug');
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->integer('discount');
+            $table->string('expires_at');
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('subcategories');
+        Schema::dropIfExists('coupons');
     }
 };
